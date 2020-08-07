e0782e2dd3ece93e46945db6ef6d7e4ef5d1d75d
Jonathan <lalilaloe@hotmail.com>
2020-08-07 14:57:25 +0200

    Subvariant test on alert

--- components/alert.ds.js
+++ components/alert.ds.js
@@ -1,20 +1,18 @@
 export default {
-  displayName: "alert",
-  name: "alert",
-  homepage:
-    "https://www.patternfly.org/v4//documentation/core/components/alert/",
-};
+    displayName: "alert",
+    name: "alert",
+    homepage: "https://www.patternfly.org/v4//documentation/core/components/alert/"
+}
 
 export const variants = [
-  {
-    displayName: "inline-types",
-    picture: {
-      src: "./pictures/alert/inline-types.png",
-      width: 293,
-      height: 404,
-    },
-    snippet: {
-      html: `<div class="pf-c-alert pf-m-inline" aria-label="Inline default alert">
+{
+displayName:"Inline default alert",
+picture:{
+src:"./pictures/alert/inline-default-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`<div class="pf-c-alert pf-m-inline" aria-label="Inline default alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-bell" aria-hidden="true"></i>
   </div>
@@ -23,7 +21,15 @@ export const variants = [
       <span class="pf-screen-reader">Default inline alert:</span>Default inline alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Inline information alert",
+picture:{
+src:"./pictures/alert/inline-information-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-info pf-m-inline" aria-label="Inline information alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
@@ -33,7 +39,15 @@ export const variants = [
       <span class="pf-screen-reader">Info alert:</span>Info inline alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Inline success alert",
+picture:{
+src:"./pictures/alert/inline-success-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Inline success alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
@@ -43,7 +57,15 @@ export const variants = [
       <span class="pf-screen-reader">Success alert:</span>Success inline alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Inline warning alert",
+picture:{
+src:"./pictures/alert/inline-warning-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-warning pf-m-inline" aria-label="Inline warning alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-exclamation-triangle" aria-hidden="true"></i>
@@ -53,7 +75,15 @@ export const variants = [
       <span class="pf-screen-reader">Warning alert:</span>Warning inline alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Inline danger alert",
+picture:{
+src:"./pictures/alert/inline-danger-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-danger pf-m-inline" aria-label="Inline danger alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-exclamation-circle" aria-hidden="true"></i>
@@ -62,18 +92,15 @@ export const variants = [
     <strong>
       <span class="pf-screen-reader">Danger alert:</span>Danger inline alert title</strong>
   </p>
-</div>`,
-    },
-  },
-  {
-    displayName: "inline-variations",
-    picture: {
-      src: "./pictures/alert/inline-variations.png",
-      width: 692,
-      height: 437,
-    },
-    snippet: {
-      html: `<div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Success alert">
+</div>`}},
+{
+displayName:"Success alert",
+picture:{
+src:"./pictures/alert/success-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`<div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Success alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
   </div>
@@ -94,66 +121,15 @@ export const variants = [
     <button class="pf-c-button pf-m-link pf-m-inline" type="button">Ignore</button>
   </div>
 </div>
-<br />
-<div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-  <div class="pf-c-alert__action">
-    <button class="pf-c-button pf-m-plain" type="button" aria-label="Close success alert: Success alert title">
-      <i class="fas fa-times" aria-hidden="true"></i>
-    </button>
-  </div>
-  <div class="pf-c-alert__description">
-    <p>Success alert description. This should tell the user more information about the alert.
-      <a href="#">This is a link.</a>
-    </p>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-  <div class="pf-c-alert__action">
-    <button class="pf-c-button pf-m-plain" type="button" aria-label="Close success alert: Success alert title">
-      <i class="fas fa-times" aria-hidden="true"></i>
-    </button>
-  </div>
-  <div class="pf-c-alert__action-group">
-    <button class="pf-c-button pf-m-link pf-m-inline" type="button">View details</button>
-    <button class="pf-c-button pf-m-link pf-m-inline" type="button">Ignore</button>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success pf-m-inline" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-</div>`,
-    },
-  },
-  {
-    displayName: "types",
-    picture: {
-      src: "./pictures/alert/types.png",
-      width: 293,
-      height: 404,
-    },
-    snippet: {
-      html: `<div class="pf-c-alert" aria-label="Default alert">
+`}},
+{
+displayName:"Default alert",
+picture:{
+src:"./pictures/alert/default-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`<div class="pf-c-alert" aria-label="Default alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-bell" aria-hidden="true"></i>
   </div>
@@ -162,7 +138,15 @@ export const variants = [
       <span class="pf-screen-reader">Default alert:</span>Default alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Information alert",
+picture:{
+src:"./pictures/alert/information-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-info" aria-label="Information alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-info-circle" aria-hidden="true"></i>
@@ -172,7 +156,15 @@ export const variants = [
       <span class="pf-screen-reader">Info alert:</span>Info alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Success alert",
+picture:{
+src:"./pictures/alert/success-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-success" aria-label="Success alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
@@ -182,7 +174,15 @@ export const variants = [
       <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Warning alert",
+picture:{
+src:"./pictures/alert/warning-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-warning" aria-label="Warning alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-exclamation-triangle" aria-hidden="true"></i>
@@ -192,7 +192,15 @@ export const variants = [
       <span class="pf-screen-reader">Warning alert:</span>Warning alert title</strong>
   </p>
 </div>
-<br />
+`}},
+{
+displayName:"Danger alert",
+picture:{
+src:"./pictures/alert/danger-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`
 <div class="pf-c-alert pf-m-danger" aria-label="Danger alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-exclamation-circle" aria-hidden="true"></i>
@@ -201,18 +209,15 @@ export const variants = [
     <strong>
       <span class="pf-screen-reader">Danger alert:</span>Danger alert title</strong>
   </p>
-</div>`,
-    },
-  },
-  {
-    displayName: "variations",
-    picture: {
-      src: "./pictures/alert/variations.png",
-      width: 855,
-      height: 766,
-    },
-    snippet: {
-      html: `<div class="pf-c-alert pf-m-success" aria-label="Success alert">
+</div>`}},
+{
+displayName:"Success alert",
+picture:{
+src:"./pictures/alert/success-alert.png",
+width:261,
+height:55},
+snippet:{
+html:`<div class="pf-c-alert pf-m-success" aria-label="Success alert">
   <div class="pf-c-alert__icon">
     <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
   </div>
@@ -233,96 +238,5 @@ export const variants = [
     <button class="pf-c-button pf-m-link pf-m-inline" type="button">Ignore</button>
   </div>
 </div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-  <div class="pf-c-alert__action">
-    <button class="pf-c-button pf-m-plain" type="button" aria-label="Close success alert: Success alert title">
-      <i class="fas fa-times" aria-hidden="true"></i>
-    </button>
-  </div>
-  <div class="pf-c-alert__description">
-    <p>Success alert description. This should tell the user more information about the alert.
-      <a href="#">This is a link.</a>
-    </p>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-  <div class="pf-c-alert__action">
-    <button class="pf-c-button pf-m-plain" type="button" aria-label="Close success alert: Success alert title">
-      <i class="fas fa-times" aria-hidden="true"></i>
-    </button>
-  </div>
-  <div class="pf-c-alert__action-group">
-    <button class="pf-c-button pf-m-link pf-m-inline" type="button">View details</button>
-    <button class="pf-c-button pf-m-link pf-m-inline" type="button">Ignore</button>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-  <div class="pf-c-alert__action">
-    <button class="pf-c-button pf-m-plain" type="button" aria-label="Close success alert: Success alert title">
-      <i class="fas fa-times" aria-hidden="true"></i>
-    </button>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Success alert title</strong>
-  </p>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert with title truncation">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title pf-m-truncate">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque cursus enim fringilla tincidunt. Proin lobortis aliquam dictum. Nam vel ullamcorper nulla, nec blandit dolor. Vivamus pellentesque neque justo, nec accumsan nulla rhoncus id. Suspendisse mollis, tortor quis faucibus volutpat, sem leo fringilla turpis, ac lacinia augue metus in nulla. Cras vestibulum lacinia orci. Pellentesque sodales consequat interdum. Sed porttitor tincidunt metus nec iaculis. Pellentesque non commodo justo. Morbi feugiat rhoncus neque, vitae facilisis diam aliquam nec. Sed dapibus vitae quam at tristique. Nunc vel commodo mi. Mauris et rhoncus leo.</strong>
-  </p>
-  <div class="pf-c-alert__description">
-    <p>This example uses ".pf-m-truncate" to limit the title to a single line and truncate any overflow text with ellipses.</p>
-  </div>
-</div>
-<br />
-<div class="pf-c-alert pf-m-success" aria-label="Success alert with title truncation at 2 lines">
-  <div class="pf-c-alert__icon">
-    <i class="fas fa-fw fa-check-circle" aria-hidden="true"></i>
-  </div>
-  <p class="pf-c-alert__title pf-m-truncate" style="--pf-c-alert__title--max-lines: 2">
-    <strong>
-      <span class="pf-screen-reader">Success alert:</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque cursus enim fringilla tincidunt. Proin lobortis aliquam dictum. Nam vel ullamcorper nulla, nec blandit dolor. Vivamus pellentesque neque justo, nec accumsan nulla rhoncus id. Suspendisse mollis, tortor quis faucibus volutpat, sem leo fringilla turpis, ac lacinia augue metus in nulla. Cras vestibulum lacinia orci. Pellentesque sodales consequat interdum. Sed porttitor tincidunt metus nec iaculis. Pellentesque non commodo justo. Morbi feugiat rhoncus neque, vitae facilisis diam aliquam nec. Sed dapibus vitae quam at tristique. Nunc vel commodo mi. Mauris et rhoncus leo.</strong>
-  </p>
-  <div class="pf-c-alert__description">
-    <p>This example uses ".pf-m-truncate" and sets "--pf-c-alert__title--max-lines: 2" to limit title to two lines and truncate any overflow text with ellipses.</p>
-  </div>
-</div>`,
-    },
-  },
-];
+`}}
+]
--- convert/convert.js
+++ convert/convert.js
@@ -5,6 +5,7 @@ const path = require('path');
 const slash = require('slash');
 const he = require('he');
 const sizeOf = require('image-size');
+const parseHtml = require('node-html-parser').default;
 
 const parse = (file) => {
     return jsonfile.readFileSync(file);
@@ -37,7 +38,7 @@ function buildVariantJson(component) {
             {
                 displayName: variant.name,
                 picture: {
-                    src: `./${variant.screenshot.url}`.replace("//", "/"), // Compatible with windows
+                    src: `./${variant.screenshot.url}`.replace("//", "/"), // Compatible with windows replacint double slashes
                     width: variant.screenshot.width,
                     height: variant.screenshot.height
                 },
@@ -52,11 +53,11 @@ ${variants.join(',\n')}
 }
 
 async function notReady(component) {
-    if (component.includes("modal")) {
+    if (component.includes("modal")) { // Check for direct component url ready
         if (!await page.$(".ws-fullscreen-example div")) {
             return true
         }
-    } else {
+    } else { // Check for component preview box ready
         if (!await page.$(".ws-preview-html")) {
             return true
         }
@@ -68,6 +69,71 @@ async function ready(component) {
     while (await notReady(component)) {
         await new Promise(r => setTimeout(r, 500)) // Wait for page to be ready
     }
+}
+
+function hasSubVariants(pageData) {
+    return pageData.result.pageContext.code.includes("<br />");
+}
+
+async function takeScreenshot(component, variant, screenshotUrl, target) {
+    const screenshotPath = path.join(screenshotDir, component)
+    const screenshotFileName = variant + ".png"
+    const screenshotFullPath = path.join(screenshotPath, screenshotFileName)
+
+    if (!fs.existsSync(screenshotFullPath)) {
+        if (await page.url() != screenshotUrl) { // If not already on page
+            console.log("     Browsing to:", screenshotUrl);
+            page.removeAllListeners(); // Avoid MaxListenersExceededWarning that crashes the script when used in large badges
+
+            await page.goto(screenshotUrl, { waitUntil: 'load', timeout: 0 }).catch(error => {
+                console.log("ERROR: Can't reach page", screenshotUrl)
+                console.log("Are you sure the patterfly docs are locally running? Start it by running npm run start:patternfly")
+            })
+        }
+        console.log("   -", variant) // Log variant name
+        await ready(component);
+
+        let element;
+        if (component.includes("modal")) { // If modal go to (isolated) component page
+            element = await page.$(".ws-fullscreen-example div") // Select child div to avoid white-space
+        } else {
+            await page.evaluate(function () {
+                var examples = document.getElementsByClassName('ws-example');
+                for (var i = 0; i < examples.length; i++) {
+                    examples[i].style.display = "inline-block" // Changes preview container size to fit component size
+                }
+                var titles = document.getElementsByClassName('pf-c-title');
+                for (var i = 0; i < titles.length; i++) {
+                    titles[i].style.display = "none" // Remove titles to scale preview box down to component size
+                }
+            })
+
+            element = await page.$(target)
+        }
+        if (!fs.existsSync(screenshotPath)) {
+            fs.mkdirSync(screenshotPath, { recursive: true });
+        }
+
+        //await page.screenshot({ path: screenshotFullPath })
+        if (element) {
+            await element.screenshot({ path: screenshotFullPath })
+            //console.log("     Screenshot:", screenshotFullPath)
+            process.stdout.write(" ✅ - " + variant + "\n\033[0G"); // The last bit replaces the whole line
+        } else {
+            process.stdout.write(" ❌ - " + variant + "\n\033[0G")
+            return false;
+        }
+    } else {
+        //console.log("     Screenshot exist:", screenshotFullPath)
+        process.stdout.write(" ❎ - " + variant + "\n\033[0G");
+    }
+    return screenshotFullPath
+}
+
+function decodeHtml(html) {
+    return he.decode(html) // decode html for export to file
+        .replace(/\/assets/g, 'node_modules/@patternfly/patternfly/assets') // Make assets resolve correctly when using snipit
+        .replace(/`/g, '\\`') // Occasionally backticks are used in the snippets (ex. page component)
 }
 
 // foxr.launch({
@@ -98,7 +164,9 @@ const screenshotDir = path.join(rootDir, "/components/pictures");
     pages = await browser.pages()
     page = pages[0]; // await browser.newPage() // << gives me an error
 
-    const components = getDirectories(componentDir)// for testing use .slice(0, 5); to limit number of components processed
+    const components = getDirectories(componentDir).filter(c => c == 'alert')// for testing use .slice(0, 5); or .filter(c => c == 'name') to limit number of components processed
+    console.log("Components found", components.length)
+    console.log("Ready to process 🚀:")
     console.log(components)
     for (const component of components) {
         console.log("*", component) // Log component name
@@ -112,74 +180,52 @@ const screenshotDir = path.join(rootDir, "/components/pictures");
                 componentpath = pageData.path.slice(0, -variant.length)
             }
             let screenshotUrl = "http://localhost:8000" + componentpath
-            if (component.includes("modal")) {
+            if (component.includes("modal")) { // In case of modal go to (isolated) component url
                 screenshotUrl = "http://localhost:8000" + pageData.path;
             }
 
+            let screenshotFullPath;
+            if (hasSubVariants(pageData)) {
+                console.log("has sub variants!")
+                const subVariants = pageData.result.pageContext.code.split("<br />")
+                const subVariantsProcessed = []
+                for (const subVariant of subVariants) {
+                    const htmlSnippet = parseHtml(subVariant.replace(/\r?\n|\r/g, ""))
+                    const subVariantTarget = "." + htmlSnippet.firstChild.classNames.join(".")
+                    if (!subVariantsProcessed.includes(subVariantTarget)) {
+                        const subVariantName = htmlSnippet.firstChild.getAttribute("aria-label");
+                        console.log(subVariantTarget)
+                        screenshotFullPath = await takeScreenshot(component, subVariantName.toLowerCase().split(" ").join("-"), screenshotUrl, subVariantTarget)
 
-            const screenshotPath = path.join(screenshotDir, component)
-            const screenshotFileName = variant + ".png"
-            const screenshotFullPath = path.join(screenshotPath, screenshotFileName)
+                        const imageSize = sizeOf(screenshotFullPath);
+                        const html = `\`${decodeHtml(subVariant)}\``
+                        variantSnippets.push({
+                            name: subVariantName,
+                            screenshot: {
+                                url: slash(screenshotFullPath.replace(rootDir, '')).replace('/components/', ''),
+                                width: imageSize.width,
+                                height: imageSize.height
+                            },
+                            html: html
+                        })
+                        subVariantsProcessed.push(subVariantTarget)
+                    }
 
-            if (!fs.existsSync(screenshotFullPath)) {
-                if (await page.url() != screenshotUrl) { // If not already on page
-                    console.log("     Browsing to:", screenshotUrl);
-                    page.removeAllListeners(); // Avoid MaxListenersExceededWarning that crashes the script when used in large badges
-                    await page.goto(screenshotUrl, { waitUntil: 'load', timeout: 0 })
                 }
-                console.log("   -", variant) // Log variant name
-                await ready(component)
-                let element;
-                if (component.includes("modal")) { // If modal go to component page
-                    console.log("modal")
-                    element = await page.$(".ws-fullscreen-example div") // Select child div to avoid white-space
-                } else {
-                    await page.evaluate(function () {
-                        var examples = document.getElementsByClassName('ws-example');
-                        for (var i = 0; i < examples.length; i++) {
-                            examples[i].style.display = "inline-block" // Changes preview container size to fit component size
-                        }
-                        var titles = document.getElementsByClassName('pf-c-title');
-                        for (var i = 0; i < titles.length; i++) {
-                            titles[i].style.display = "none" // Remove titles to scale preview box down to component size
-                        }
-                    })
-
-                    element = await page.$(`#ws-core-c-${component}-${variant}`)
-                }
-                if (!fs.existsSync(screenshotPath)) {
-                    fs.mkdirSync(screenshotPath, { recursive: true });
-                }
-
-                //await page.screenshot({ path: screenshotFullPath })
-                if (element) {
-                    await element.screenshot({ path: screenshotFullPath })
-                    //console.log("     Screenshot:", screenshotFullPath)
-                    process.stdout.write(" ✅ - " + variant + "\n\033[0G"); // The last bit replaces the whole line
-                } else {
-                    process.stdout.write(" ❌ - " + variant + "\n\033[0G")
-                }
-
             } else {
-                //console.log("     Screenshot exist:", screenshotFullPath)
-                process.stdout.write(" ❎ - " + variant + "\n\033[0G");
+                screenshotFullPath = await takeScreenshot(component, variant, screenshotUrl, `#ws-core-c-${component}-${variant}`)
+                const imageSize = sizeOf(screenshotFullPath);
+                const html = `\`${decodeHtml(pageData.result.pageContext.code)}\``
+                variantSnippets.push({
+                    name: variant,
+                    screenshot: {
+                        url: slash(screenshotFullPath.replace(rootDir, '')).replace('/components/', ''),
+                        width: imageSize.width,
+                        height: imageSize.height
+                    },
+                    html: html
+                })
             }
-
-            const imageSize = sizeOf(screenshotFullPath);
-            const htmlInput = he.decode(pageData.result.pageContext.code) // decode html for export to file
-                .replace(/\/assets/g, 'node_modules/@patternfly/patternfly/assets') // Make assets resolve correctly when using snipit
-                .replace(/`/g, '\\`') // Occasionally backticks are used in the snippets (ex. page component)
-            const html = `\`${htmlInput}\``
-            variantSnippets.push({
-                name: variant,
-                screenshot: {
-                    url: slash(screenshotFullPath.replace(rootDir, '')).replace('/components/', ''),
-                    width: imageSize.width,
-                    height: imageSize.height
-                },
-                html: html
-            })
-
         }
         const outputComponent = buildComponentJson({
             name: component,
